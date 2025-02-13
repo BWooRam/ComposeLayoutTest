9140f5837fa0d6f2d70abb307dbb6bc1287c340a
BWooRam <wrbae@hyundaiht.co.kr>
2025-02-13 17:00:08 +0900

    테스트 UI 추가

--- app/src/main/java/com/hyundaiht/composelayouttest/ui/WeekdaySelector.kt
+++ app/src/main/java/com/hyundaiht/composelayouttest/ui/WeekdaySelector.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 val koreanDays = listOf("일", "월", "화", "수", "목", "금", "토")
@@ -31,21 +32,24 @@ val ChineseDays = listOf("一", "二", "三", "四", "五", "六", "日")
 fun WeekdaySelector(
     days: List<String>,
     selectedDays: List<String>,
+    onDaySelected: (String) -> Unit,
+    daySize: Dp = 40.dp,
+    daySpaceBy: Dp = 8.dp,
     defaultColor: Color = Color.White,
-    selectedColor: Color = Color.Black,
-    onDaySelected: (String) -> Unit
+    activeColor: Color = Color.Black,
+    borderColor: Color = Color.Black
 ) {
     Row(
-        horizontalArrangement = Arrangement.spacedBy(8.dp), modifier = Modifier.padding(16.dp)
+        horizontalArrangement = Arrangement.spacedBy(daySpaceBy), modifier = Modifier.padding(16.dp)
     ) {
         days.forEach { day ->
             val isSelected = selectedDays.contains(day)
             Box(contentAlignment = Alignment.Center,
                 modifier = Modifier
-                    .size(40.dp)
+                    .size(daySize)
                     .clip(RoundedCornerShape(8.dp))
-                    .border(2.dp, Color.Black, RoundedCornerShape(8.dp))
-                    .background(if (isSelected) selectedColor else defaultColor)
+                    .border(2.dp, borderColor, RoundedCornerShape(8.dp))
+                    .background(if (isSelected) activeColor else defaultColor)
                     .clickable { onDaySelected(day) }) {
                 Text(
                     text = day,
@@ -63,13 +67,14 @@ fun WeekdaySelectorScreen(onSelectedDay: (List<String>) -> Unit = {}) {
 
     WeekdaySelector(
         days = koreanDays,
-        selectedDays = selectedDays
-    ) { day ->
-        selectedDays = if (day in selectedDays) {
-            selectedDays - day // 선택 해제
-        } else {
-            selectedDays + day // 선택
+        selectedDays = selectedDays,
+        onDaySelected = { day ->
+            selectedDays = if (day in selectedDays) {
+                selectedDays - day // 선택 해제
+            } else {
+                selectedDays + day // 선택
+            }
+            onSelectedDay.invoke(selectedDays)
         }
-        onSelectedDay.invoke(selectedDays)
-    }
+    )
 }
