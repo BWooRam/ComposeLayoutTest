885e3509e21332f02873852dd560954f07d191c0
BWooRam <wrbae@hyundaiht.co.kr>
2025-02-13 17:30:00 +0900

    테스트 UI 추가

--- app/src/main/java/com/hyundaiht/composelayouttest/ui/Tag.kt
+++ app/src/main/java/com/hyundaiht/composelayouttest/ui/Tag.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
@@ -18,7 +17,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
@@ -39,8 +37,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -79,8 +75,7 @@ fun TagInputField(
                         .weight(1f)
                         .defaultMinSize(minHeight = 40.dp)
                         .wrapContentHeight()
-                        .align(Alignment.CenterVertically)
-                        .background(Color.Magenta),
+                        .align(Alignment.CenterVertically),
                     verticalArrangement = Arrangement.spacedBy(4.dp),
                     horizontalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
@@ -119,8 +114,7 @@ fun TagInputField(
                     IconButton(
                         modifier = Modifier
                             .size(40.dp, 40.dp)
-                            .align(Alignment.CenterVertically)
-                            .background(Color.Cyan),
+                            .align(Alignment.CenterVertically),
                         onClick = onClearTag
                     ) {
                         Icon(imageVector = Icons.Default.Close, contentDescription = "Clear tags")
